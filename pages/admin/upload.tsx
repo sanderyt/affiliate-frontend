55a979aca35f861f242b3214fbbd81c78fab33ed
Sander van Rijsoort <hallo@svanrijsoort.nl>
2021-03-31 21:42:47 +0100

    added upload button

--- pages/admin/upload.tsx
+++ pages/admin/upload.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { LayoutAdmin } from "../../components/LayoutAdmin";
+import { Button, Grid } from "@material-ui/core";
 
 const Upload = () => {
   return (
     <LayoutAdmin>
-      <h1>Upload page</h1>
+      <Grid container justify="center" direction="column" alignItems="center">
+        <h1>Upload page</h1>
+        <Button variant="contained" component="label" color="primary">
+          Upload File
+          <input type="file" hidden />
+        </Button>
+      </Grid>
     </LayoutAdmin>
   );
 };
