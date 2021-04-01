de67b5045b925e28bdf7d7a0a28bf6e3f249d774
Sander van Rijsoort <hallo@svanrijsoort.nl>
2021-04-01 23:28:30 +0100

    added basic file validation for uploading productfeed

--- pages/admin/upload.tsx
+++ pages/admin/upload.tsx
@@ -1,24 +1,43 @@
 import React, { useState, ChangeEvent } from "react";
 import { LayoutAdmin } from "../../components/LayoutAdmin";
-import { Button, Grid } from "@material-ui/core";
+import { Button, Grid, Snackbar } from "@material-ui/core";
 import { Publish } from "@material-ui/icons";
 
 const Upload = () => {
   const [files, setFiles] = useState(null);
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const fileReader = new FileReader();
+    const uploadedFile = e.target.files && e.target.files[0];
 
-    if (e.target.files) {
-      fileReader.readAsText(e.target.files[0], "UTF-8");
-      fileReader.onload = e => {
-        setFiles(e.target.result);
-        const results = e.target.result;
-
-        console.log(results, "result");
-      };
+    if (uploadedFile) {
+      if (validateFile(uploadedFile)) {
+        fileReader.readAsText(e.target.files[0], "UTF-8");
+        fileReader.onload = e => {
+          setFiles(e.target.result);
+          const results = e.target.result;
+        };
+      } else {
+        setError(true);
+        setErrorMessage("This is not a JSON file");
+      }
     }
   };
+
+  const validateFile = (file: any) => {
+    const allowedExtensions = ["json"];
+    const fileExtension = file.name.split(".").pop();
+
+    if (!allowedExtensions.includes(fileExtension)) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  const handleClose = () => setError(false);
 
   return (
     <LayoutAdmin>
@@ -27,9 +46,22 @@ const Upload = () => {
         <Button variant="contained" component="label" color="primary">
           <Publish />
           Upload File
-          <input type="file" hidden onChange={handleChange} />
+          <input
+            type="file"
+            hidden
+            onChange={handleChange}
+            accept="application/JSON"
+          />
         </Button>
         {files && <h1>Files uploaded</h1>}
+        {error && (
+          <Snackbar
+            open={error}
+            autoHideDuration={6000}
+            onClose={handleClose}
+            message={errorMessage}
+          />
+        )}
       </Grid>
     </LayoutAdmin>
   );
