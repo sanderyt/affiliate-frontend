4ec710074dab57c60c0e49df9b907e2f13fe242f
Sander van Rijsoort <hallo@svanrijsoort.nl>
2021-04-02 18:43:35 +0100

    added react hook form

--- pages/admin/navigation.tsx
+++ pages/admin/navigation.tsx
@@ -1,18 +1,35 @@
 import React, { FC } from "react";
-import { Grid } from "@material-ui/core";
+import { useForm } from "react-hook-form";
+import { Grid, TextField, Button } from "@material-ui/core";
+import { Add } from "@material-ui/icons";
 
 import { LayoutAdmin } from "../../components/LayoutAdmin";
 import { ContentBox } from "../../components/ContentBox";
 
-interface Props {}
+interface Inputs {}
 
 const Navigation: FC<Props> = ({}) => {
+  const { register, handleSubmit, watch, errors } = useForm<Inputs>();
+  const onSubmit = data => console.log(data);
+
   return (
     <LayoutAdmin>
       <Grid container justify="center" direction="column" alignItems="center">
         <h2>Navigatie</h2>
         <div>Je hebt nog geen navigatie, voeg je eerste categorie toe!</div>
-        <ContentBox>Hallo</ContentBox>
+        <ContentBox>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <TextField
+              id="outlined-basic"
+              label="Categorie"
+              variant="outlined"
+            />
+            <Button variant="contained" component="label" color="primary">
+              <Add />
+              Toevoegen
+            </Button>
+          </form>
+        </ContentBox>
       </Grid>
     </LayoutAdmin>
   );
